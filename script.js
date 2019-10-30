e7c86dd9434633d83088dfc1e6c7923261006be3
Logan Uibel <lbuibel@gmail.com>
2019-10-29 20:32:32 -0600

    added herritage section with transitions

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// var grid = document.querySelector('.grid');
+// var msnry = new Masonry( grid, {
+//   // options...
+//   itemSelector: '.grid-item',
+//   columnWidth: 200
+// });
+
+// // init with selector
+// var msnry = new Masonry( '.grid', {
+//   // options...
+// });
+
+/////////////////////////////////////////////
+
 const controller = new ScrollMagic.Controller();
 
 var scene = new ScrollMagic.Scene({
@@ -26,4 +40,25 @@ var scene3 = new ScrollMagic.Scene({
     .addTo(controller)
     .addIndicators({
         name: 'yolo'
+    })
+
+var scene4 = new ScrollMagic.Scene({
+    triggerElement: '.classic-heading',
+        triggerHook: "onLeave",
+
+})
+    .setClassToggle('.section4-heading', 'section4-heading-fade')
+    .addTo(controller)
+    .addIndicators({
+        name: 'new'
+    })
+
+var scene5 = new ScrollMagic.Scene({
+    triggerElement: '.classic-heading',
+        triggerHook: "onLeave",
+})
+    .setClassToggle('.section4-text', 'section4-text-fade')
+    .addTo(controller)
+    .addIndicators({
+        name: 'new new'
     })
