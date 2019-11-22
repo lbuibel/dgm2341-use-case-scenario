384aa100f0a63f74dbf13c0d2f944b6a91a99c26
Logan Uibel <lbuibel@gmail.com>
2019-11-22 12:49:45 -0700

    total overhaul

--- script.js
+++ script.js
@@ -1,58 +1,86 @@
-// selector string as first argument
+//register the scroll event
+window.addEventListener('scroll', onScroll, false);
+
+//Get all the section reference
+var sectionOne = document.querySelector('.item-1');
+var sectionTwo = document.querySelector('.item-2');
+var sectionThree = document.querySelector('.item-3');
+var sectionForth = document.querySelector('.item-4');
+var sectionFivth = document.querySelector('.item-5');
+
+//Calculate their individual height
+var SectionOneHeight = getComputedStyle(sectionOne).height.split('px')[0];
+var SectionTwoHeight = getComputedStyle(sectionTwo).height.split('px')[0];
+var SectionThreeHeight = getComputedStyle(sectionThree).height.split('px')[0];
+var SectionForthHeight = getComputedStyle(sectionForth).height.split('px')[0];
+var SectionFivthHeight = getComputedStyle(sectionFivth).height.split('px')[0];
+
+//calculate the checkpoint where item need to be modified
+var checkPointOne = parseFloat(SectionOneHeight);
+var checkPointTwo = checkPointOne + parseFloat(SectionTwoHeight);
+var checkPointThree = checkPointTwo + parseFloat(SectionThreeHeight);
+var checkPointForth = checkPointThree + parseFloat(SectionForthHeight);
+
+//Scroll logic
+function onScroll() {
+  //get the current scrollbar position
+  var scrollBarPosition = window.pageYOffset | document.body.scrollTop
+  console.log(scrollBarPosition + ' : ' + checkPointOne)
+    //First section is already fixed
+  if (scrollBarPosition >= 0 && scrollBarPosition < checkPointOne) {
+    removeClass(sectionTwo, sectionThree)
+  } else if (scrollBarPosition > checkPointOne && scrollBarPosition <= checkPointTwo) {
+    addClass(sectionTwo, sectionThree, checkPointTwo)
+    removeClass(sectionThree, sectionForth)
+  } else if (scrollBarPosition > checkPointTwo && scrollBarPosition <= checkPointThree) {
+    addClass(sectionThree, sectionForth, checkPointThree)
+    removeClass(sectionForth, sectionFivth)
+  } else if (scrollBarPosition > checkPointThree && scrollBarPosition <= checkPointForth) {
+    addClass(sectionForth, sectionFivth, checkPointForth)
+  }
+
+}
+
+function addClass(elemOne, elemTwo, margin) {
+  elemOne.classList.add('fixed');
+  elemTwo.style.marginTop = (margin) + 'px';
+}
+
+function removeClass(elemOne, elemTwo) {
+  elemOne.classList.remove('fixed');
+  elemTwo.style.marginTop = '0px';
+}
+
+
+//--------------------  MASONRY ------------------- //
 var colc = new Colcade( '.grid', {
-  columns: '.grid-col',
-  items: '.grid-item'
-});
+    columns: '.grid-col',
+    items: '.grid-item'
+  });
 
-/////////////////////////////////////////////
-
+//------------------ SCROLL MAGIC -----------------//
 const controller = new ScrollMagic.Controller();
 
-var scene = new ScrollMagic.Scene({
-    triggerElement: '#section-2', // scene starts when this element is reached
+var scene1 = new ScrollMagic.Scene({
+    triggerElement: '.wrapper',    
 })
-    .setClassToggle('#boxer-img', 'fade-in')
+    .setClassToggle('.story-heading', 'story-heading-fade')
     .addTo(controller)
-        .addIndicators()
 
 
 var scene2 = new ScrollMagic.Scene({
-    triggerElement: '#section-2'
+    triggerElement: '.wrapper',
 })
-    .setClassToggle('.boxerinfo', 'boxer-fade')
+    .setClassToggle('.story-text', 'story-text-fade')
     .addTo(controller)
-        .addIndicators({
-            indent: 200,
-            name: 'test'
-        })
+    classic-heading
 
-        
-var scene3 = new ScrollMagic.Scene({
-    triggerElement: '#section-3'
-})
-    .setClassToggle('.classic-heading', 'classic-fade')
-    .addTo(controller)
-    .addIndicators({
-        name: 'yolo'
+
+
+    var scene3 = new ScrollMagic.Scene({
+        triggerElement: '.wrapper',
     })
-
-var scene4 = new ScrollMagic.Scene({
-    triggerElement: '.classic-heading',
-        triggerHook: "onLeave",
-
-})
-    .setClassToggle('.section4-heading', 'section4-heading-fade')
-    .addTo(controller)
-    .addIndicators({
-        name: 'new'
-    })
-
-var scene5 = new ScrollMagic.Scene({
-    triggerElement: '.classic-heading',
-        triggerHook: "onLeave",
-})
-    .setClassToggle('.section4-text', 'section4-text-fade')
-    .addTo(controller)
-    .addIndicators({
-        name: 'new new'
-    })
+        .setClassToggle('.boxer-img', 'boxer-img-fade')
+        .addTo(controller)
+        classic-heading
+    
